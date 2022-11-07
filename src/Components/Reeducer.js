5cb874e7e3a2638ea30f5b1276c7d391db419669
Christian Ofoka <104909904+Codename-Kris@users.noreply.github.com>
2022-11-07 12:40:12 +0100

    initial commit

--- src/Components/Reeducer.js
+++ src/Components/Reeducer.js
@@ -11,43 +11,4 @@ const Reducer = () => {
   );
 };
 
-//const initialState = { count: 0 };
-// };
-// default:
-// return { count: state.count };
-//}
-//}
-
-//const Counter = () => {
-//const [state, dispatch] = useReducer(reducer, initialState);
-
-//return (
-//<div>
-//<input type="number" disabled value={state.count} className="input" />
-
-//<br />
-//<br />
-//<button
-//className="btn incrementBtn"
-//onClick={() => dispatch({ type: "increment" })}
-// >
-// Increment
-//</button>
-//<button
-//className="btn decrementBtn"
-// onClick={() => dispatch({ type: "decrement" })}
-//>
-//Decrement
-//</button>
-//<br />
-//<button
-// className="btn resetBtn"
-// onClick={() => dispatch({ type: "reset" })}
-//>
-// Reset
-//</button>
-// </div>
-//);
-//};
-
 export default Reducer;
